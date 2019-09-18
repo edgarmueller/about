199d909057e4484484cffb82f0d1dcd36b68d331
Edgar Mueller <edgar.m.mueller@gmail.com>
2019-09-18 23:51:44 +0200

    Clear footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,6 @@ import React from "react";
 
 const Footer = () => (
   <div style={{ textAlign: "center" }}>
-    <h1>🙏 Stay in Touch</h1>
-    <p>
-      <a href="https://twitter.com/amanhimself">Twitter</a>,&nbsp;
-      <a href="mailto:abc@xyz.com">Email</a>
-    </p>
     <br />
   </div>
 );
