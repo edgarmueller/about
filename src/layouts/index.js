c9a79609a937f90ae97a37755568366db3c35fe5
Edgar Mueller <edgar.m.mueller@gmail.com>
2019-09-19 00:14:22 +0200

    Remove some meta info

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -12,17 +12,6 @@ const TemplateWrapper = ({ children }) => (
       <meta name="author" content={author.name} />
       <meta name="description" content={author.description} />
       <meta name="Keywords" content={site.Keywords} />
-
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:title" content={author.name} />
-      <meta name="twitter:description" content={author.description} />
-      <meta name="twitter:creator" content={twitter.creator} />
-
-      <meta name="og:title" content={og.title} />
-      <meta name="og:description" content={og.description} />
-      <meta name="og:url" content={og.url} />
-      <meta name="og:site_name" content={og.site_name} />
-      <meta name="og:type" content={og.type} />
     </Helmet>
     {children()}
   </div>
