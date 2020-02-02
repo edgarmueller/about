48f27cd30c1dbdc431fe61f8573bd5548d23b0fd
Edgar Mueller <edgar.m.mueller@gmail.com>
2020-02-02 22:58:55 +0100

    Update general about info
    * Update `gatsby` package

--- content/projects/jsonforms/index.md
+++ content/projects/jsonforms/index.md
@@ -1,9 +1,9 @@
 ---
 title: JSONForms
-date: 2018-current
+date: 2018-2019
 path: /projects/jsonforms
 URL: jsonforms.io
 type: professional
 ---
 
-React-based framework for rendering forms based on JSON schema and a UI description
+React-based framework for rendering forms based on JSON schema and a UI description. 
--- src/components/About.js
+++ src/components/About.js
@@ -4,12 +4,12 @@ const noListStyle = { listStyle: 'none' }
 
 const About = () => (
   <div>
-    <h2><span role="img" aria-label="current status">👉</span> Current status</h2>
+    <h2><span role="img" aria-label="current status">👉</span>Current status</h2>
     <ul style={noListStyle}>
-      <li>Munich </li>
-      <li>Working @ EclipseSource <span role="img" aria-label="programming">💻</span></li>
-      <li>Current topics: JS, TS, React-stack, Web tooling</li>
-      <li>Past topics: Java, Eclipse, EMF, OSGi, Scala</li>
+      <li>Munich</li>
+      <li>Working @ <a href="https://coachhub.io">coachhub</a> <span role="img" aria-label="programming">💻</span></li>
+      <li>Current topics: Microservices, NestJS, Typescript</li>
+      <li>Past topics: Model driven development, (Web) Tooling, JSON Schema, Java, Scala, OSGI, Eclipse Platform</li>
     </ul>
   </div>
 );
